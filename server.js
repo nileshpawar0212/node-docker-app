868b3c22f45e1f6b10619190c6ee3413fcde75cd
nileshpawar0212 <nileshpawar0212@gmail.com>
2025-09-28 02:09:26 +0530

    new changes added

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send('Hello from Node.js running i K8S!'));
+app.get('/', (req, res) => res.send('Hello from Node.js running in K8S!'));
 
 app.listen(PORT, () => console.log(`Server listening on http://localhost:${PORT}`));
 
